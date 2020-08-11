c3271c1be89c5377fd339f3b1d233a40891d9eff
Justin_Howard <zorro1rr@gmail.com>
2020-08-10 18:09:50 -0600

    Updates

---
+++ precache-manifest.2592ebf9099f252fceac45be02e153f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d2313031b05950020718058a02b4a4",
+    "url": "/Mighty-Tasty/index.html"
+  },
+  {
+    "revision": "4f32588528a2f73a468b",
+    "url": "/Mighty-Tasty/static/css/main.2d4ac752.chunk.css"
+  },
+  {
+    "revision": "30a0f0ab9fe859ef4143",
+    "url": "/Mighty-Tasty/static/js/2.93edab62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mighty-Tasty/static/js/2.93edab62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f32588528a2f73a468b",
+    "url": "/Mighty-Tasty/static/js/main.8ff289dd.chunk.js"
+  },
+  {
+    "revision": "2163bfdfa9eee7a387f5",
+    "url": "/Mighty-Tasty/static/js/runtime-main.0fa4f4dc.js"
+  },
+  {
+    "revision": "a2e4555cad6aea6537c0ba7cd7a3abd5",
+    "url": "/Mighty-Tasty/static/media/background.a2e4555c.gif"
+  },
+  {
+    "revision": "83942fdc46cb8b6a22ca6f6bd2b1e428",
+    "url": "/Mighty-Tasty/static/media/favicon.83942fdc.ico"
+  },
+  {
+    "revision": "e36b4ad96f9296f7d584df6134ef0885",
+    "url": "/Mighty-Tasty/static/media/searchy.e36b4ad9.gif"
+  }
+]);
